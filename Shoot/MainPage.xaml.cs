e8d2235163c1de66adcebabf45a8b7552bf82959
edewit <erikjan.dewit@gmail.com>
2014-12-23 09:22:23 +0100

    initial version shoot and share

---
+++ Shoot/MainPage.xaml.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Graphics.Display;
+using Windows.Media.Capture;
+using Windows.Media.MediaProperties;
+using Windows.Storage;
+using Windows.Storage.Streams;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=391641
+
+namespace Shoot
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private MediaCapture captureMgr { get; set; }
+
+        public MainPage()
+        {
+            Window.Current.SizeChanged += Current_SizeChanged;
+            this.InitializeComponent();
+
+            this.NavigationCacheMode = NavigationCacheMode.Required;
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.
+        /// This parameter is typically used to configure the page.</param>
+        protected async override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            // TODO: Prepare page for display here.
+
+            // TODO: If your application contains multiple pages, ensure that you are
+            // handling the hardware Back button by registering for the
+            // Windows.Phone.UI.Input.HardwareButtons.BackPressed event.
+            // If you are using the NavigationHelper provided by some templates,
+            // this event is handled for you.
+
+            captureMgr = new MediaCapture();
+            await captureMgr.InitializeAsync();
+            capturePreview.Source = captureMgr;
+            await captureMgr.StartPreviewAsync();
+
+            DisplayInformation displayInfo = DisplayInformation.GetForCurrentView();
+            displayInfo.OrientationChanged += DisplayInfo_OrientationChanged;
+            DisplayInfo_OrientationChanged(displayInfo, null);
+        }
+
+        private async void TakePicture(object sender, RoutedEventArgs e)
+        {
+            var encodingProperties = ImageEncodingProperties.CreateJpeg();
+            StorageFile file = await ApplicationData.Current.LocalFolder.CreateFileAsync("Photo.jpg", CreationCollisionOption.ReplaceExisting);
+
+            await captureMgr.CapturePhotoToStorageFileAsync(encodingProperties, file);
+            var bitmap = new BitmapImage();
+            bitmap.SetSource(await file.OpenReadAsync());
+            shot.Source = bitmap;
+
+            VisualStateManager.GoToState(this, "Taken", true);
+        }
+
+        void Current_SizeChanged(object sender, Windows.UI.Core.WindowSizeChangedEventArgs e)
+        {
+            var CurrentViewState = ApplicationView.GetForCurrentView().Orientation;
+            switch (CurrentViewState)
+            {
+                case ApplicationViewOrientation.Landscape:
+                    grid.RowDefinitions.Clear();
+                    captureMgr.SetPreviewRotation(VideoRotation.Clockwise180Degrees);
+                    grid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(1, GridUnitType.Star) });
+                    grid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(177) });
+                    shareBar.ColumnDefinitions.Clear();
+                    for (int i = 0; i < 3; i++)
+                        shareBar.RowDefinitions.Add(new RowDefinition() { Height = new GridLength(1, GridUnitType.Star) });
+                    break;
+
+                case ApplicationViewOrientation.Portrait:
+                    captureMgr.SetPreviewRotation(VideoRotation.Clockwise90Degrees);
+                    grid.ColumnDefinitions.Clear();
+                    grid.RowDefinitions.Add(new RowDefinition() { Height = new GridLength(1, GridUnitType.Star) });
+                    grid.RowDefinitions.Add(new RowDefinition() { Height = new GridLength(177) });
+                    shareBar.RowDefinitions.Clear();
+                    for (int i = 0; i < 3; i++)
+                        shareBar.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(1, GridUnitType.Star) });
+
+                    break;
+            }
+        }
+
+        private void DisplayInfo_OrientationChanged(DisplayInformation sender, object args)
+        {
+            captureMgr.SetPreviewRotation(VideoRotationLookup(sender.CurrentOrientation, false));
+        }
+
+        private VideoRotation VideoRotationLookup(DisplayOrientations displayOrientation, bool counterclockwise)
+        {
+            switch (displayOrientation)
+            {
+                case DisplayOrientations.Landscape:
+                    return VideoRotation.None;
+
+                case DisplayOrientations.Portrait:
+                    return (counterclockwise) ? VideoRotation.Clockwise270Degrees : VideoRotation.Clockwise90Degrees;
+
+                case DisplayOrientations.LandscapeFlipped:
+                    return VideoRotation.Clockwise180Degrees;
+
+                case DisplayOrientations.PortraitFlipped:
+                    return (counterclockwise) ? VideoRotation.Clockwise90Degrees :
+                    VideoRotation.Clockwise270Degrees;
+
+                default:
+                    return VideoRotation.None;
+            }
+        }
+    }
+}
